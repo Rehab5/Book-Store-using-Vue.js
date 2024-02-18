2a9e4883cb57015beda415159ac060e6fb5b45dc
Rehab Kosbar <69490304+Rehab5@users.noreply.github.com>
2024-02-18 21:45:31 +0200

    Add files via upload

---
+++ books.js
@@ -0,0 +1,73 @@
+let books = [{
+    ISBN:"9780061120084",
+    name:"To Kill a Mockingbird",
+    category:"Fiction",
+    image:"https://cdn2.penguin.com.au/covers/original/9781785151552.jpg",
+    noOfPages:336,
+    auther:"Harper Lee",
+    price:100
+},
+{
+    ISBN:"9780141439563",
+    name:"1984",
+    category:"Fiction",
+    image:"https://m.media-amazon.com/images/I/7180qjGSgDL._AC_UF1000,1000_QL80_.jpg",
+    noOfPages:328,
+    auther:"George Orwell",
+    price:70
+},
+{
+    ISBN:"9780679783268",
+    name:"The Catcher in the Rye",
+    category:"Fiction",
+    image:"https://cdn.britannica.com/94/181394-050-2F76F7EE/Reproduction-cover-edition-The-Catcher-in-the.jpg",
+    noOfPages:224,
+    auther:"J.D. Salinger",
+    price:85
+},
+{
+    ISBN:"9780385519259",
+    name:"The Alchemist",
+    category:"Fiction",
+    image:"https://cdn.aseeralkotb.com/storage/media/130031/conversions/the-alchemist-1154-200x300-jpg.jpg",
+    noOfPages:150,
+    auther:"paulo coelho",
+    price:70
+},
+{
+    ISBN:"9780385260436",
+    name:"Cities of Salt",
+    category:"Fiction",
+    image:"https://arabicbookworm.files.wordpress.com/2017/06/d9a2d9a6-d985d8afd986-d8a7d984d985d984d8ad-d9a4-d9a1.jpg",
+    noOfPages:512,
+    auther:"Abdul Rahman Munif",
+    price:120
+},
+{
+    ISBN:"9780385722205",
+    name:"The Yacoubian Building",
+    category:"Fiction",
+    image:"https://m.media-amazon.com/images/I/71Ho2ekUy5L._AC_UF894,1000_QL80_.jpg",
+    noOfPages:272,
+    auther:"Alaa Al Aswany",
+    price:70
+},
+{
+    ISBN:"9780385333849",
+    name:"A Thousand Splendid Suns",
+    category:"Fiction",
+    image:"https://images.penguinrandomhouse.com/cover/9780735218246",
+    noOfPages:419,
+    auther:"Khaled Hosseini",
+    price:90
+},
+{
+    ISBN:"9780061120084",
+    name:"The Power of Now",
+    category:"Self-Help",
+    image:"https://m.media-amazon.com/images/I/71I6MaZsCcL._AC_UF1000,1000_QL80_.jpg",
+    noOfPages:236,
+    auther:"Eckhart Tolle",
+    price:130
+}
+]
